6fc224a31a0f906ce29948cd186cc9eb4bb86820
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-13 11:55:14 -0600

    Last componet migrated to the context

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,26 +1,20 @@
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { ActivityActions } from "../reducers/activityReducer"
-import { Dispatch, useMemo } from "react"
+import { useMemo } from "react"
 import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline"
+import { useActicity } from "../hooks/useActivity"
 
-
-type ActivityListProps = {
-    activities: Activity[]
-    dispatch: Dispatch<ActivityActions>
-}
-
-export default function ActivityList({ activities, dispatch }: ActivityListProps) {
-
-    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
-    const isEmpty = useMemo(() => activities.length === 0, [activities])
+export default function ActivityList() {
+    const { state, dispatch } = useActicity()
+    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [state.activities])
+    const isEmpty = useMemo(() => state.activities.length === 0, [state.activities])
 
     return (
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">Comia y Actividades</h2>
 
             {isEmpty ? <p className="text-center my-3">No hay datos para mostrar...</p> :
-                activities.map(activity => (
+                state.activities.map(activity => (
                     <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                         <div className="space-y-2 relative">
                             <p className={`absolute -top-10 -left-5 p-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-green-400' : 'bg-orange-500'}`}>{categoryName(+activity.category)}</p>
