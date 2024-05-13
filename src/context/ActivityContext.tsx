759fa3e4135c21ce3d83e0c1a0e6e55efa767402
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-13 11:50:50 -0600

    Migrating derivated states to the context

--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -5,6 +5,7 @@ import {
   activityReducer,
   initialState,
 } from "../reducers/activityReducer";
+import { useMemo } from "react"
 
 type ActivityProviderProps = {
   children: ReactNode;
@@ -13,15 +14,19 @@ type ActivityProviderProps = {
 type ActivityContextProps = {
   state: ActivityState;
   dispatch: Dispatch<ActivityActions>;
+  caloriesConsumed: number
+  caloriesBurned: number
 };
 
 export const ActivityContext = createContext<ActivityContextProps>(null!);
 
 export const ActivityProvider = ({ children }: ActivityProviderProps) => {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+  const caloriesConsumed = useMemo(() => state.activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [state.activities])
+  const caloriesBurned = useMemo(() => state.activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [state.activities])
 
   return (
-    <ActivityContext.Provider value={{ state, dispatch }}>
+    <ActivityContext.Provider value={{ state, dispatch, caloriesConsumed, caloriesBurned }}>
       {children}
     </ActivityContext.Provider>
   );
